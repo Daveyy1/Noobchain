3b3e73d55aa77f554e12a9314b8a3f8b25812913
Daveyy1 <155442164+Daveyy1@users.noreply.github.com>
2025-09-04 15:05:44 +0200

    Implemented a method to check if the blockchain by calculating the current and previous hash of a block and comparing it to the actual value. Implemented a mine method where an integer value is hashed until it equals the has value of a block (nonce).

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String previousHash;
     private String data; // data here will be a message, normally a transaction
     private long timeStamp; // number of milliseconds from 1/1/1970
+    private int nonce;
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -17,8 +18,20 @@ public class Block {
         String calculatedhash = StringUtil.applySHA256(
                 previousHash +
                         Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
                         data
         );
         return calculatedhash;
+    }
+
+    // difficulty is the number of 0's the target hash must contain at the beginning, typical values for modern currencies
+    // like Litecoin start at around 420000
+    public void mineBlock(int difficulty){
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target)){
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined successfully. : " + hash);
     }
 }
--- src/main/java/Noobchain.java
+++ src/main/java/Noobchain.java
@@ -1,14 +1,58 @@
+import java.util.ArrayList;
+import com.google.gson.*;
+
 public class Noobchain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 6;
+
     public static void main(String[] args) {
 
-        Block GenesisBlock = new Block("Genesis Block", "0");
-        System.out.println("Hash for block 1: " + GenesisBlock.hash);
+        //add blocks to the blockchain
+        blockchain.add(new Block("Genesis Block", "0"));
+        System.out.println("Trying to Mine block 1... (this may take a while) ");
+        blockchain.get(0).mineBlock(difficulty);
 
-        Block secondBlock = new Block("Second Block", GenesisBlock.hash);
-        System.out.println("Hash for block 2: " + secondBlock.hash);
 
-        Block thirdBlock = new Block("Third Block", secondBlock.hash);
-        System.out.println("Hash for block 3: " + thirdBlock.hash);
+        blockchain.add(new Block("Block 2", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... (this may take a while) ");
+        blockchain.get(1).mineBlock(difficulty);
 
+
+        blockchain.add(new Block("Block 3", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... (this may take a while) ");
+        blockchain.get(2).mineBlock(difficulty);
+
+
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
+        System.out.println(blockchainJson);
+    }
+
+    // checks if the current blockchain is valid, if there were any changes made to the blocks, this method will return false
+    public static Boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        // loop through blockchain to check hashes
+        for(int i = 1; i < blockchain.size(); i++){
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            // compare the registered hash with the calculated hash
+            if(!currentBlock.hash.equals(currentBlock.calculateHash())){
+                System.out.println("Current Hashes not equal");
+                return false;
+            } else if (!previousBlock.hash.equals(currentBlock.previousHash)){
+                System.out.println("Previous Hashes not equal");
+                return false;
+            } else if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)){
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
+        }
+        return true;
     }
 }
